732b9988216ef53fb8d5b31f2711edbeef53b211
James Hopkins <james@idreamincode.co.uk>
2014-06-19 23:48:14 +0100

    An implementation of a circular buffer so we can easily move between cardinal points

--- scripts/robot.js
+++ scripts/robot.js
@@ -2,20 +2,69 @@ function Robot(command) {
 
     var position = command.split('\n');
 
-    this.commands = {
-        L: 'turned left',
-        R: 'turned right',
-        F: 'moved forward'
+    this.movementTypes = {
+        L: {
+            direction: 'left',
+            message: 'turned left'
+        },
+        R: {
+            direction: 'right',
+            message: 'turned right'
+        },
+        F: {
+            direction: 'forward',
+            message: 'moved forward'
+        }
     };
+
+    /*this.cardinalPoints = {
+        'N': 'north',
+        'E': 'east',
+        'S': 'south',
+        'W': 'west'
+     };*/
+
+    this.cardinalPoints = ['N', 'E', 'S', 'W'];
+
     this.gridCoordinates = position[0].split(' ');
 
-    this.startMovementSequence(position[1]);
+    var initialCardinalPoint = this.gridCoordinates[2];
+
+    //console.log('Robot activated, positioned facing ' + this.cardinalPoints[this.initialCardinalPoint]);
+    console.log('Robot activated');
+
+    this.startMovementSequence(initialCardinalPoint, position[1]);
 
 }
 
-Robot.prototype.startMovementSequence = function(sequence) {
-    for ( var i = 0; i < sequence.length; i++ ) {
+/**
+ * 
+ */
+Robot.prototype.createCircularBufferOfCardinalPoints = function(point, direction) {
+    var pointIndex = this.cardinalPoints.indexOf(point);
+    var hello;
+    switch (direction) {
+        case 'L':
+            pointIndex = pointIndex - 1;
+            break;
+        case 'R':
+            pointIndex = pointIndex + 1;
+            break;
+    };
+    if (pointIndex >= this.cardinalPoints.length) {
+        pointIndex = 0;
+    };
+    if (pointIndex == -1) {
+        pointIndex = this.cardinalPoints.length - 1;
+    }
+    return this.cardinalPoints[pointIndex];
+};
+
+Robot.prototype.startMovementSequence = function(initialCardinalPoint, sequence) {
+    var initialCardinalPoint = initialCardinalPoint;
+    for ( var i = 0; i < 3; i++ ) {
         var movement = sequence.charAt(i);
-        console.log(movement);
+        initialCardinalPoint = this.createCircularBufferOfCardinalPoints(initialCardinalPoint, movement);
+        console.log(initialCardinalPoint);
     };
 };
